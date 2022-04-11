a2e56e05a4c8824c1ae2e0a31c82d7532da5e474
OriginalCube <lancemiraflorcube@gmail.com>
2022-04-11 14:50:40 +0800

    Added new route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get('', (req,res)=>{
+  res.send('Hello world!');
+});
+
 app.get('/api', (req,res)=>{
     res.send('This Will Change!');
 });
