3826b72a880d5f682af57210426b32ab20c38dd0
OriginalCube <lancemiraflorcube@gmail.com>
2022-04-11 14:54:22 +0800

    Server.js Updated to test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/', (req,res)=>{
-  res.send('Hello world!');
+  res.send('Hello world!#2');
 });
 
 app.get('/api', (req,res)=>{
