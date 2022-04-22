91cafb9ac808f2c7a1d095fc28cfdf39e2a94cab
OriginalCube <lancemiraflorcube@gmail.com>
2022-04-22 18:39:10 +0800

    Course Project hello

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/', (req,res)=>{
-  res.send('Course project!!');
+  res.send('Hello Course Project!!');
 });
 
 app.get('/api', (req,res)=>{
